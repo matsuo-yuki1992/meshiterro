23da90625c68c45b2d3809be95ba14447b8ac9e8
EC2 Default User <padosyu1127@gmail.com>
2024-07-07 03:26:11 +0000

    [finish]4-17

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
- resources :users, only: [:show, :edit]
+ 
+ devise_for :users
+ resources :users, only: [:show, :edit, :update]
  resources :post_images, only: [:new, :index, :show, :create, :destroy]
-  devise_for :users
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 
 root to: "homes#top"
