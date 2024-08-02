d596b6d497fb4a302fa405da3d56ab2a79163c98
EC2 Default User <padosyu1127@gmail.com>
2024-08-02 11:33:17 +0000

    [finish]5-2

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,13 @@ Rails.application.routes.draw do
   devise_for :admin, skip: [:registrations, :password], controllers: {
     sessions: 'admin/sessions'
   }
+
+  
+  namespace :admin do
+    get "dashboards", to: "dashboards#index"
+    resources :users, only: [:destroy]
+  end
+  
  devise_for :users
  resources :users, only: [:show, :edit, :update]
  resources :post_images, only: [:new, :index, :show, :create, :destroy] do
